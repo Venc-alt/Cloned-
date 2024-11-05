0133633dac4e6f06fffd671ff032786fdc154c7e
Vasny Navas <vasny@interprenet.net>
2024-11-04 23:17:49 -0600

    going back to atlas with new updates

--- server.js
+++ server.js
@@ -1,35 +1,3 @@
-// const express = require('express');
-// const cors = require('cors');
-// const connectDB = require('./config/db');
-// const userRoutes = require('./routes/userRoutes');
-// const gatewayRoutes = require('./routes/gatewayRoutes');
-// const reservationRoutes = require('./routes/reservationRoutes');
-
-// const app = express();
-
-
-// app.use(cors({
-//   origin: 'https://available-gateways-frontend-51573bdecab0.herokuapp.com',
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-//   credentials: true
-// }));
-
-
-// app.use(express.json());
-
-// // Routes
-// app.use('/api/users', userRoutes);
-// app.use('/api/gateways', gatewayRoutes);
-// app.use('/api/reservations', reservationRoutes);
-
-// connectDB();
-
-// app.listen(process.env.PORT || 5000, () => {
-//     console.log(`Server is running on port ${process.env.PORT || 5000}`);
-// });
-
-
 
 const express = require('express');
 const cors = require('cors');
@@ -43,7 +11,7 @@ const app = express();
 
 // CORS setup for frontend connection
 app.use(cors({
-  origin: 'https://available-gateways-frontend-51573bdecab0.herokuapp.com',
+  origin: 'https://available-gateways-frontend-51573bdecab0.herokuapp.com0',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
